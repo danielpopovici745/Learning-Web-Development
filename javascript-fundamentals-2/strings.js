7804920731978e5bd1bead1b6e9eb353be75be45
Daniel Popovici <dpopovici601@gmail.com>
2022-03-11 21:45:18 -0500

    going over handling text in javascript

---
+++ javascript-fundamentals-2/strings.js
@@ -0,0 +1,27 @@
+const string = 'The revolution will not be televised.';
+console.log(string);
+
+const badString = string;
+console.log(badString);
+
+const sgl = 'Single quotes.';
+const dbl = "Double quotes.";
+console.log(sgl);
+console.log(dbl);
+
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+console.log(sglDbl);
+console.log(dblSgl);
+
+const bigmouth = 'I\'ve got no right to take my place...';
+console.log(bigmouth);
+
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log(greeting);
+
+const one = "Hello,";
+const two = "how are you?";
+const joined = `${one} ${two}`;
+console.log(joined);
