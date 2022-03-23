d28603c6510986d66663dae40263eefc5d72111f
Daniel Popovici <dpopovici601@gmail.com>
2022-03-23 19:05:46 -0400

    if, else if, and else practice.

---
+++ javascript-fundamentals-2/condtionals.js
@@ -0,0 +1,26 @@
+// The if Statement
+
+if (true == 1){
+    console.log('if statement');
+}
+
+// The else statement
+
+if (true != 1){
+    console.log('if statement');
+}
+else{
+    console.log('else statement!');
+}
+
+// The else if statement
+
+if (true != 1){
+    console.log('if statement');
+}
+else if(true == 1){
+    console.log('else if statement!')
+}
+else{
+    console.log('this gets executed if the else if statement above is false')
+}
