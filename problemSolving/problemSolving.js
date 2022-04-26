3c664aa95b8f6a8bd8d15f238ade0eeb8dec9068
Daniel Popovici <dpopovici601@gmail.com>
2022-04-25 23:27:09 -0400

    created new docs for lesson on effective problem solving.

---
+++ problemSolving/problemSolving.js
@@ -0,0 +1,41 @@
+// Problem Solving
+
+/* The best way to improve your problem solving ability is by buidling experience by making lots and lots of programs. The more practice you have the better you'll be prepared to solve real world problems.*/
+
+// Understand the Problem
+
+/* The first step to solving a problem is understanding exactly what the problem is. If you don't understand the problem you won't know when you've successfully solved it.
+
+To gain clarity and understanding of the problem, write it down on paper,reword it in plain English until it makes sense to you, draw diagrams if it helps. When yu can explain the problem to someone in plain English, you understand it.
+*/
+
+//Plan
+
+/* Now that you know what you're aiming to solve, don't jump into coding just yet. It's time to plan out how you're going to solve it first. 
+
+Some of the questions you should answer at this stage of the process:
+
+    Does your program have a user interface? What will it look like? What functionality will the interface have? Sketch it out on paper.
+
+    What inputs will your program have? Will the user enter data or will you get input from somewhere else?
+
+    What's the desired output?
+
+    Given your inputs, what are the steps necessary to return the desired output?
+
+The last question is where you will write out an algorithm to solve the problem. You can think of an algorithm as a recipe for solving a particular problem. It defines the steps that need to be taken by the computer to solve a problem in pseudocode.
+*/
+
+// Pseudocode
+
+/* Pseudocode is writing out the logic for your program in natural language instead of code. It helps you slow down and think through the steps your program will have to go through to solve the problem.
+
+Example of what pseudocode for a simple program that prints all numbers up to an inputted number might look like:
+
+    When the user inputs a number, initalize a counter variable and set its value, while counter is smaller than user inputted number, print the value of the counter value
+*/
+
+// Divide and Conquer
+
+/* From your planning, you should have identified some subproblems of the big problem you're solving. Each of the steps in the algorithm
+
