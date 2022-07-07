db66f6fbc3dd332091279637b394688e23d87b42
Daniel Popovici <dpopovici601@gmail.com>
2022-07-07 19:52:40 -0400

    solved first step of pseudocode which involves the user inputting a number.

--- problemSolving/problemSolving.js
+++ problemSolving/problemSolving.js
@@ -39,3 +39,36 @@ Example of what pseudocode for a simple program that prints all numbers up to an
 
 /* From your planning, you should have identified some subproblems of the big problem you're solving. Each of the steps in the algorithm
 */
+
+// FizzBuzz problem
+
+/* Write a program that takes a user's input and prints the numbers from one to the number the user entered. However, for multiples of three prind out Fizz instead of the number and for multiple of five print out Buzz. For numbers which are multiple of three and five print out fizzbuzz
+*/
+
+// Reword the problem if neccessary
+
+/* Write a program that allows the user to enter a number, print each number from 1 to the user's number. Any number that is divisible by three without a remainder print out Fizz instead. Any number that is divisible by five without a remainder print out Buzz instead. Any number divisble by 3 and 5 without a remainder print out FizzBuzz instead.*/
+
+// Plan
+
+/* Does your program have an interface? What will it look like? 
+    An interface isn't necessary since it will be completed in the browser console. 
+
+What inputs will your program have? Will the user enter data or will you input data from somewhere else? 
+    The user will enter a number from a prompt
+
+What's the desired output?
+    The desired output is a list of numbers from 1 to the user's number. Except each number that is divisible by 3 will print out Fizz instead. Each number divisible by 5 will print Buzz instead and each number divisible by 3 and 5 will print FizzBuzz instead.
+
+Given your inputs, what are the steps necessary to return the desired output? 
+    The algorithm in pseudocode for this problem:
+
+    User inputs a number
+    Loop from 1 to the entered number
+    If the current loop iteration number is divisible by 3 then print Fizz
+    If the current loop iteration is divisible by 5 then print Buzz
+    If the current loop iteration is divisible by 3 and 5 print out FizzBuzz
+    Otherwise, print the current loop iteration
+*/
+
+let answer = parseInt(prompt('Please enter a number you would like to FizzBuzz up to: '))
