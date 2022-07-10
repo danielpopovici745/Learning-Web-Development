70f04cbff988e344d95e27f7fd478fcabae53982
Daniel Popovici <dpopovici601@gmail.com>
2022-07-10 09:09:35 -0400

    finished working through the lesson, still need to complete the assignment on the mdn web docs.

---
+++ understandingErrors/script.js
@@ -0,0 +1,25 @@
+// An error is a type of object built into the JS language, consisting of a name/type and a message
+
+const a = "Hello";
+const b = "World";
+
+// console.log(c);
+
+// This example of an error is a ReferenceError. It is thrown when one refers to a variable that is not declared and/or initialized within the current scope. 
+
+// In the error message it will give the name of the file and the location of where the problematic code is.
+
+//Another important part of an error is the stack trace
+
+const c = 5;
+const d = 6;
+
+function add(){
+    return e;
+}
+
+function print(){
+    add();
+}
+
+print();
