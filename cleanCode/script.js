3555cf03c186370c598c346b5934d5673b28a2c0
Daniel Popovici <dpopovici601@gmail.com>
2022-07-16 06:58:23 -0400

    finished the clean code lesson writing some notes on it.

---
+++ cleanCode/script.js
@@ -0,0 +1,12 @@
+/* Top 3 things to remember
+
+1. Know how to distinguish hard to read code from easy to read code
+
+2. Use programming principles to make your code cleaner
+
+3. Write good comments
+*/
+
+// Explain why code works not how code works.//
+
+//Rewrite logic/code in your program before writing comments.
